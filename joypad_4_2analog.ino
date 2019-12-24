0feba02c5be519a50a9b0f1d1e8590f4f03215cd
SergioPoverony <sergiopoverony@gmail.com>
2019-12-24 20:43:13 +0500

    Prepared
    Prepared only for test

--- joypad_4_2analog.ino
+++ joypad_4_2analog.ino
@@ -1,11 +1,14 @@
 /* Not tested code */
 #include <Joystick.h>
 #define PINS 14
+
 #define ENABLE_ANALOG true
 int X1 = A0;
 int Y1 = A1;
 int X2 = A2;
 int Y2 = A3;
+int BatterPin = 23;
+
 /*
  * 18 = A0
  * 19 = A1
@@ -16,7 +19,7 @@ int Y2 = A3;
 // Create Joystick
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_MULTI_AXIS, PINS, 0,
   true, true, false, true, true, false, false, false, false, false, true);
- 
+  
 class CButton {
   public:
   int pin = 0;
@@ -30,10 +33,14 @@ class CButton {
 CButton Buttons[PINS] ={0,1,2,3,4,5,6,7,8,9,10,16,14,15};
  
 void setup() {
+//open console for battery status
+   Serial.begin(9600);
+
+//check pins  
   for(int i=0 ; i<PINS ;i++) {
     pinMode(Buttons[i].pin, INPUT_PULLUP);
   }
- 
+
   Joystick.begin();
   if (ENABLE_ANALOG) {
     Joystick.setXAxisRange(256, -256);
@@ -61,8 +68,16 @@ void JButtonStates() {
     }
   }  
 }
- 
+
+void BatteryStatus()
+{
+  int sensorValue = analogRead(BatterPin); //read the A5 pin value
+  float voltage = sensorValue * (5.00 / 1024.00) * 100; //convert the value to a true voltage.
+  Serial.println(voltage); //print the voltage to tty console
+}
+
 void loop() {
   JButtonStates();
-  delay(50);
+  BatteryStatus(); 
+  delay(25);
 }
